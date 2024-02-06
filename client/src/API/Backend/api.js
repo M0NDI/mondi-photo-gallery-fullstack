50e289a5ffedf85562aa5fde3aedcf67e3f7b8c1
m0ndi <armond_93@live.co.uk>
2024-02-06 19:21:53 +0000

    .

--- client/src/API/Backend/api.js
+++ client/src/API/Backend/api.js
@@ -1,5 +1,13 @@
 import axios from "axios";
 
+//Development urls
+
+/* const authUrl = "http://localhost:3000/api/v1/auth";
+const usersUrl = "http://localhost:3000/api/v1/users";
+const imagesUrl = "http://localhost:3000/api/v1/images";
+ */
+
+// production urls
 const authUrl = "https://mondi-photo-gallery-api.onrender.com/api/v1/auth";
 const usersUrl = "https://mondi-photo-gallery-api.onrender.com/api/v1/users";
 const imagesUrl = "https://mondi-photo-gallery-api.onrender.com/api/v1/images";
