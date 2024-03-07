b80549c8e5f5a17fac61cda7f3f0e8bd9662e7e2
m0ndi <armond_93@live.co.uk>
2024-03-07 17:44:44 +0000

    changed threshold needed for infinite scroll api call to be triggered from 0.5 to 0.6

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -126,7 +126,7 @@ function App() {
       if (loading) return;
       const handleScroll = _.debounce(() => {
         const pageHeight = document.documentElement.scrollHeight - window.innerHeight;
-        if (window.scrollY / pageHeight >= 0.5) {
+        if (window.scrollY / pageHeight >= 0.6) {
           getNextPage();
         }
       }, 150);
